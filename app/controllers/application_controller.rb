2a72f2f4980785898aa0767b31a687e7511bd930
Getu Gebre <getu.gebre.gg@gmail.com>
2020-11-10 16:54:48 -0600

    refactor code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,45 +13,39 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/articles/:id' do 
-    id = params[:id].to_i
-    @article = Article.find(id)
-    erb :show
-  end
-
   get '/articles/new' do 
     erb :new
   end
 
+  get '/articles/:id' do 
+    @article = Article.find_by(id: params[:id].to_i)
+    erb :show
+  end
+
+  
   get '/articles/:id/edit' do 
     @article = Article.find(params[:id].to_i)
     erb :edit
   end
 
   patch '/articles/:id' do 
-    puts params[:title]
-    # find by id
-    # update the retrieved article with the data
+  
     article = Article.find_by(id: params[:id] )
-    article.update(title: params[:title], content: params[:content])
-    @article = Article.find_by(id: params[:id] )
-    erb :show
+    article.update(params[:article])
+    
+  
+    redirect "/articles/#{article.id}"
   end
 
   post '/articles' do 
-    title = params[:title]
-    content = params[:content]
-    new_article = Article.create(title: title, content: content)
-    @articles = Article.all
+   
+    article = Article.create(params[:article])
+    redirect "/articles/#{article[:id]}"
     
-    erb :index
   end
 
   delete '/articles/:id' do 
-      article = Article.find_by(id: params[:id].to_i)
-      article.destroy
-
-      @articles = Article.all
-      erb :index
+    Article.find_by(id: params[:id].to_i).destroy 
+    redirect "/articles"
   end
 end
